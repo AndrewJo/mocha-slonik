3d3b1141ae19d1c40a6a9db852d9215c91651cda
Andrew Jo <andrew@verdigris.co>
2021-10-14 10:37:34 -0700

    Implement mochaHooks

---
+++ src/register.ts
@@ -0,0 +1,24 @@
+import { RootHookObject } from "mocha";
+import { ImportMock, OtherManager } from "ts-mock-imports";
+import * as bindPool from "slonik/dist/src/binders/bindPool";
+import { BindPoolFunction, BindPoolMock } from "mocha-slonik/bindPoolMock";
+
+let bindPoolMock: BindPoolMock;
+let bindPoolImportMock: OtherManager<BindPoolFunction>;
+
+export const mochaHooks: RootHookObject = {
+  beforeAll() {
+    bindPoolMock = new BindPoolMock();
+    bindPoolImportMock = ImportMock.mockOther(
+      bindPool,
+      "bindPool",
+      bindPoolMock.bindPool()
+    );
+  },
+  afterEach() {
+    bindPoolMock.rollback();
+  },
+  afterAll() {
+    bindPoolImportMock.restore();
+  }
+};
